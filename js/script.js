611dba7236c48b30ade7319d5b94650c56d3ac11
willa <willa_abreu@outlook.com>
2019-04-11 21:50:40 -0300

    fixed bg

--- js/script.js
+++ js/script.js
@@ -8,3 +8,10 @@ $(document).ready(function() {
 
 
 });
+
+/*
+var http = require("http");
+setInterval(function() {
+    http.get("http://<your app name>.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
+*/
